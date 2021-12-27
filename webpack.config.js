49985da1af32906efaefdac5c05dced5852d8c6e
Sebastian Pape <s@pape.io>
2021-12-27 15:08:47 +0100

    v1.0.18: fix proper packaging esm+umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
-module.exports = {
+const path = require('path');
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+
+const umd = {
   output: {
     filename: 'index.js',
     library: 'WalletConnect',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist/umd')
   },
   node: {
     Buffer: true
   }
 }
+
+const esm = {
+  output: {
+    filename: 'index.js',
+    library: 'WalletConnect',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'dist/esm')
+  },
+  plugins: [
+    new EsmWebpackPlugin()
+  ],
+  node: {
+    Buffer: true
+  }
+}
+
+module.exports = [umd, esm]
